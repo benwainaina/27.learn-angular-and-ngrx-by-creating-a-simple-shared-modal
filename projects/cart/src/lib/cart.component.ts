4c89245c90760d655e4a87517461c062b18fb2e9
ben wainaina <bwainainamwangi@gmail.com>
2024-12-09 15:30:32 +0300

    feat: finalize cart UI

--- projects/cart/src/lib/cart.component.ts
+++ projects/cart/src/lib/cart.component.ts
@@ -4,13 +4,20 @@ import { Component } from '@angular/core';
   selector: 'lib-cart',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: 'cart.component.html',
+  styleUrl: 'cart.component.scss',
 })
 export class CartComponent {
+  public cartItems: Array<{ sku: string; name: string; price: number }> = [
+    { sku: '1', name: 'Milk', price: this._randomPrice() },
+    { sku: '2', name: 'Eggs', price: this._randomPrice() },
+    { sku: '3', name: 'Oranges', price: this._randomPrice() },
+    { sku: '4', name: 'Flour', price: this._randomPrice() },
+  ];
 
+  private _randomPrice(): number {
+    return (
+      Math.floor(Math.random() * 1000) + Math.ceil(Math.random() * 100) / 100
+    );
+  }
 }
